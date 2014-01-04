04afde3719d8d1f2b8859a518314db15da1e90f8
David BOUREAU <davidboureau@administrateurSFEIRs-MacBook-Pro.local>
2014-01-04 10:42:10 +0100

    improved storage adapter

--- example/localStorageAdapter.js
+++ example/localStorageAdapter.js
@@ -3,24 +3,54 @@ var _setItem = Storage.prototype.setItem;
 var currentTodos = [];
 
 Storage.prototype.setItem = function(key, value) {
-	if (this === window.localStorage) {
-         // do what you want if setItem is called on localStorage
-         console.log("setItem " + key + " " + value);
+   if (this === window.localStorage) {
+      // do what you want if setItem is called on localStorage
+      console.log("setItem " + key + " " + value);
 
-         var currentValue = JSON.parse(value);
+      var currentValues = JSON.parse(value);
 
-         if (currentValue.length === currentTodos.length + 1) {
-          $.ajax({
+      if (currentValues.length === currentTodos.length + 1) {  // addition
+
+         var addedItem = _.last(currentValues);
+         addItem(key, addedItem);
+
+         $.ajax({
             url: "/todos",
             type: "POST",
-            data: JSON.stringify(_.last(currentValue))
-         }).done(function() {
+            data: JSON.stringify(_.last(currentValues))
+         });
 
+      } else if (currentValues.length === currentTodos.length) { // modification
+         var modifiedValue =  _.find(currentValues, function(e) {return _.isEmpty(_.where(currentTodos, e));});
+         $.ajax({
+            url: "/todos",
+            type: "PUT",
+            data: JSON.stringify(modifiedValue)
          });
       }
-      currentTodos = currentValue;
+
+
+      currentTodos = currentValues;
    } else {
-         // fallback to default action
-         _setItem.apply(this, arguments);
-      }
-   };
+      // fallback to default action
+      _setItem.apply(this, arguments);
+   }
+};
+
+
+/*
+ *
+ * Format correctly a new item according to the framework used
+ *
+ */
+var addItem = function (framework, item) {
+   switch (framework) {
+      case "todos-jquery":
+         item.id = Date.now();
+         break;
+      default:
+         throw {
+            message: "unknow framework " + framework
+         };
+   }
+});
